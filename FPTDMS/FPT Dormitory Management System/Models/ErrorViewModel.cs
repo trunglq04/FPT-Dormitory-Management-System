3fc0f9a0209862d1b7a3b0753a0581b915566b4c
Louis28 <louis280104@gmail.com>
2024-06-11 16:43:56 +0700

    Create Project

--- FPTDMS/FPT Dormitory Management System/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace FPT_Dormitory_Management_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
