9985b1d205e6270feca90c5a88fd2ac9c9ed52f3
Louis28 <louis280104@gmail.com>
2024-06-11 17:21:13 +0700

    Add semantic ui react for client-app

--- FPTDMS/client-app/src/main.tsx
+++ FPTDMS/client-app/src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/layout/App.tsx'
+import 'semantic-ui-css/semantic.min.css'
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
