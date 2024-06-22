7f28c19c8417265cd532ecac22626c708c5fb2f6
thanhnhan <nhandthe17016@fpt.edu.vn>
2024-06-22 20:33:21 +0700

    Update DTO

---
+++ FPTDMS/DMS_API/DMS_API/Models/DTO/FloorDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace DMS_API.Models.DTO
+{
+    public class FloorDTO
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public Guid DormId { get; set; }
+
+        //// Dorm DTO
+        //public DormDto Dorm { get; set; }
+
+        // Nested DTOs for related entities
+        public List<HouseDTO> Houses { get; set; }
+    }
+}
