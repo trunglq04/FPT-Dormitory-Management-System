f44dabcdad0de07a9783dd631f2e4ea1004210a2
thanhnhan <nhandthe17016@fpt.edu.vn>
2024-06-22 20:09:49 +0700

    Room Update

--- FPTDMS/DMS_API/DMS_API/Models/Domain/Room.cs
+++ FPTDMS/DMS_API/DMS_API/Models/Domain/Room.cs
@@ -3,7 +3,6 @@
     public class Room
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
         public string Status { get; set; }
         public string Description { get; set; }
         public int Capacity { get; set; }
