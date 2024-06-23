64454edea7b87fa141f23c5523e06ca8daea8973
thanhnhan <nhandthe17016@fpt.edu.vn>
2024-06-24 00:11:56 +0700

    Update HouseDTO

--- FPTDMS/DMS_API/DMS_API/Models/DTO/HouseDTO.cs
+++ FPTDMS/DMS_API/DMS_API/Models/DTO/HouseDTO.cs
@@ -6,6 +6,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Status { get; set; }
+        public int Capacity { get; set; }
         public string FloorName { get; set; }
 
         //// Floor DTO
