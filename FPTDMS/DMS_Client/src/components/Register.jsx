7673fd445d9ae8cd393f1b8db0b02fb31f218199
Louis28 <louis280104@gmail.com>
2024-06-27 18:02:22 +0700

    Add new prop isCompletedInfo to AppUser

--- FPTDMS/DMS_Client/src/components/Register.jsx
+++ FPTDMS/DMS_Client/src/components/Register.jsx
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { axios } from "axios";
 
 import config from "../config";
 
