38af94fe3ba99767fb6bebdf6cdc085ea151de58
thanhnhan <nhandthe17016@fpt.edu.vn>
2024-07-06 00:22:31 +0700

    Add CompleteProfile -Users will have to complete their profile after successful registration

---
+++ FPTDMS/DMS_API/DMS_API/Models/DTO/Request/AddProfileInfoRequestDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace DMS_API.Models.DTO.Request
+{
+    public class AddProfileInfoRequestDTO
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Gender { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+        public string? Address { get; set; }
+        public string? Picture { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
